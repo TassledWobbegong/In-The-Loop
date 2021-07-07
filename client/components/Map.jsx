93bd72fdfeda265892367649aadb344b4dd90fc6
aramay <kasper_341@hotmail.com>
2021-07-07 12:00:41 -0700

    pass new .env variables for configurations

---
+++ client/components/Map.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import mapbox from 'mapbox-gl';
+
+const Map = () => {
+  const [longitude, setLongitude] = useState('-74.0060');
+  const [latitude, setLatitude] = useState('40.7128');
+  const [zoom, setZoom] = useState('13');
+  const [style, setStyle] = useState('mapbox://styles/mapbox/satellite-v9');
+
+  useEffect(() => {
+    mapbox.accessToken = process.env.MAPBOXAPIKEY;
+
+      
+    const map = new mapbox.Map({
+      container: 'map',
+      style: style,
+      center: [longitude, latitude],
+      zoom: zoom,
+    });
+    map.addControl(new mapbox.NavigationControl());
+
+  });
+
+  return (
+    <div
+      id="map"
+      style={{ position: 'fixed', top: 0, left:0 , height: '100%', width: '100%'}}
+    >
+        <img src='client/images/b7d0b611d3d927b74c6b71f5e797a5fe.jpeg'/>
+    </div>
+  );
+};
+
+export default Map;
