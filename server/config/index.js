6c0a8af56b835eae4410d1cf47e1f66176f25446
aramay <kasper_341@hotmail.com>
2021-07-07 11:56:58 -0700

    update .env varaible name

--- server/config/index.js
+++ server/config/index.js
@@ -3,16 +3,17 @@ require('dotenv').config();
 const config = {
   env: process.env.NODE_ENV,
   host: process.env.HOST,
-  database: process.env.DATABASE,
-  port: process.env.PORT,
+  DATABASE: process.env.DATABASE,
+  PORT: process.env.PORT,
   googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
-  spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
-  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
   ticketmasterConsumerKey: process.env.TICKETMASTER_CONSUMER_KEY,
   ticketmasterConsumerSecret: process.env.TICKETMASTER_CONSUMER_SECRET,
   predictHqClientId: process.env.PREDICT_HQ_CLIENT_ID,
   predictHqClientSecret: process.env.PREDICT_HQ_CLIENT_SECRET,
   predictHqClientTemporaryToken: process.env.PREDICT_HQ_TEMPORARY_TOKEN,
+  MAPBOXAPIKEY: process.env.MAPBOXAPIKEY
 };
 
 module.exports = config;
